10d1c60de7b323f18b3e15e4e8d7812fca683a53
Dilasa Thapa <thapadilasa71@gmail.com>
2024-01-30 15:32:03 +0530

    added store

--- redux-counter/src/App.jsx
+++ redux-counter/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import './App.css'
 import Counter from './Component/LikeCounter.jsx'
+import reducer from './Component/Reducer.jsx'
+import { legacy_createStore as createStore } from "redux";
+
 
 function App() {
 
+  // let store = createStore(reducer)
   return (
     <>
       <div>
--- redux-counter/src/Component/LikeCounter.jsx
+++ redux-counter/src/Component/LikeCounter.jsx
@@ -5,12 +5,12 @@ import reducer from "./Reducer";
 
 // const store = createStore(reducer);
 
+let store = createStore(reducer)
 export default function Counter() {
   const [count, setCounter] = useState(0);
-    console.log(count)
-  let store = createStore(reducer)
-//   console.log(store.getState())
-
+    // console.log(count)
+    //   console.log(store.getState())
+    
   store.subscribe(() => {
     console.log("cc", store.getState().count)
     setCounter(store.getState().count);
@@ -20,8 +20,8 @@ export default function Counter() {
     <div>
       <h3>{count}</h3>
       {/* {console.log("cg", incrementNumber())} */}
-      <button onClick={() => {store.dispatch(incrementNumber())}}>Like</button>
-      <button onClick={() => {store.dispatch(decrementNumber())}}>Unlike</button>
+      <button onClick={() => store.dispatch(incrementNumber())}>Like</button>
+      <button onClick={() => store.dispatch(decrementNumber())}>Unlike</button>
     </div>
   );
 }
